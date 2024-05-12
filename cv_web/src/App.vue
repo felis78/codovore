77de68e77e49e021b6a1a699319a9f511a666e13
Guillaume ROUCHEUX <guillaume.roucheux@tutanota.com>
2024-05-12 18:17:13 +0200

    Initial commit

---
+++ cv_web/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-main>
+      <HighBar/>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+  import HighBar from './components/HighBar.vue'
+</script>
+<style scoped>
+
+*
+{
+  background: white;
+}
+
+</style>
---
+++ cv_web/src/components/HighBar.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-app-bar color: white
+      background-color="white"
+      density="comfortable"
+      height="10%">
+        <v-row justify="center">
+          <v-app-bar-title text="Guillaume Roucheux - Developpeur web fullstack"></v-app-bar-title>
+        </v-row>
+    </v-app-bar>
+</template>
+
+<script setup>
+
+</script>
